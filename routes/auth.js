8681b7a1f5eb8d9053d5eb166d29ece78c8a5adb
qapng <quanganhphucnguyen@gmail.com>
2021-08-30 23:39:40 +0700

    added feature-login-user

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const User = require('../models/User');
 router.post('/register', async (req, res) => {
   const { username, password, email } = req.body;
 
-  // Simple Validation
+  // Simple Validation for adequate information
   if (!username || !password || !email) {
     // No username or password specify
     return res
@@ -50,6 +50,55 @@ router.post('/register', async (req, res) => {
     return res.json({
       success: true,
       message: 'User created successfully',
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+// @route POST api/auth/login
+// @desc Login user
+// @access public
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Simple Validation
+  if (!username || !password) {
+    // No username or password specify
+    return res
+      .status(400)
+      .json({ success: false, message: 'Missing username and/or password' });
+  }
+  try {
+    // Check for existing user
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid username' });
+    }
+
+    // Username found check password
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid password' });
+    }
+
+    // Valid username and password, return access token
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+    );
+
+    return res.json({
+      success: true,
+      message: 'User logged in successfully',
       accessToken,
     });
   } catch (error) {
