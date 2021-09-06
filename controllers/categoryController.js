f4ae6ec3fef91ced633cbe5032021b3d64121d54
May Myat Noe Htet <honlay.mmnh@gmail.com>
2021-09-06 23:07:45 +1000

    Implement get, update and delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -32,25 +32,59 @@ exports.getCategories = async (req, res) => {
 };
 
 exports.getCategory = async (req, res) => {
-  // add logic
-  res.status(501).json({
-    status: 'error',
-    message: 'route under development'
-  });
+  try {
+    const category = await Category.findById(req.params.id);
+
+    if (category) {
+      res.status(200).json({
+        status: 'success',
+        message: { category }
+      });
+    } else {
+      res.status(404).json({
+        status: 'fail',
+        message: 'Category not found'
+      });
+    }
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.updateCategory = async (req, res) => {
-  // add logic
-  res.status(501).json({
-    status: 'error',
-    message: 'route under development'
-  });
+  try {
+    const contact = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: { contact }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.deleteCategory = async (req, res) => {
-  // add logic
-  res.status(501).json({
-    status: 'error',
-    message: 'route under development'
-  });
+  try {
+    const contact = await Category.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      message: { contact }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
