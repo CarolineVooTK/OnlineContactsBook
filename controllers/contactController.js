8cdf4541af4a07ed22038d6dff2194622f1556d3
Soe Wei Yan Phyo <soew@student.unimelb.edu.au>
2021-09-11 19:45:51 +1000

    catch all async arrors from contractController.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,93 +1,59 @@
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 const Contact = require('../models/Contact');
 
-exports.createContact = async (req, res) => {
-  try {
-    const newContact = await Contact.create(req.body);
+exports.getAllContacts = catchAsync(async (req, res, next) => {
+  // Execute query.
+  const features = new APIFeatures(Contact.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const contacts = await features.query;
 
-    res.status(201).json({
-      status: 'success',
-      data: { contact: newContact }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'error',
-      message: err
-    });
-  }
-};
+  // Send response.
+  res.status(200).json({
+    status: 'success',
+    results: contacts.length,
+    message: { contacts }
+  });
+});
 
-exports.getAllContacts = async (req, res) => {
-  try {
-    // Execute query.
-    const features = new APIFeatures(Contact.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const contacts = await features.query;
+exports.getContact = catchAsync(async (req, res, next) => {
+  const contact = await Contact.findById(req.params.id);
 
-    // Send response.
-    res.status(200).json({
-      status: 'success',
-      results: contacts.length,
-      message: { contacts }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    message: { contact }
+  });
+});
 
-exports.getContact = async (req, res) => {
-  try {
-    const contact = await Contact.findById(req.params.id);
+exports.createContact = catchAsync(async (req, res, next) => {
+  const newContact = await Contact.create(req.body);
 
-    res.status(200).json({
-      status: 'success',
-      message: { contact }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: { contact: newContact }
+  });
+});
 
-exports.updateContact = async (req, res) => {
-  try {
-    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateContact = catchAsync(async (req, res, next) => {
+  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    res.status(200).json({
-      status: 'success',
-      message: { contact }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    message: { contact }
+  });
+});
 
-exports.deleteContact = async (req, res) => {
-  try {
-    const contact = await Contact.findByIdAndDelete(req.params.id);
+exports.deleteContact = catchAsync(async (req, res, next) => {
+  const contact = await Contact.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      message: { contact }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    message: { contact }
+  });
+});
