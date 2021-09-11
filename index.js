1b9860082b35daefdd893a22f8164a281e02e0b3
Soe Wei Yan Phyo <soew@student.unimelb.edu.au>
2021-09-11 21:31:46 +1000

    Handle all unhandled rejections

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ const connectDB = async () => {
 };
 connectDB();
 
-const { PORT } = process.env;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
+});
+
+// Handle all unhandled rejections.
+process.on('unhandledRejection', err => {
+  console.log(err.name, ': ', err.message);
+  console.log('UNHANDLED REJECTIONS! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
