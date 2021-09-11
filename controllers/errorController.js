f7a12fcb4837517e1437262951bdfd366d917fa7
Soe Wei Yan Phyo <soew@student.unimelb.edu.au>
2021-09-11 20:11:34 +1000

    Send different error msgs in dev and prod environment.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,39 @@
-// Global error handling middleware.
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     error: err,
     stack: err.stack
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send msg to client.
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknown error: don't leak error details.
+  } else {
+    console.log('ERROR 💥 ', err);
+
+    // Send generic msg.
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
+// Global error handling middleware.
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
