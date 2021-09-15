fba494e413be375c07dc445e5dd65a64304ea283
Soe Wei Yan Phyo <soew@student.unimelb.edu.au>
2021-09-15 17:45:09 +1000

    Nth

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-// Handle all uncaught exceptions
+// Handle all uncaught exceptions.
 process.on('uncaughtException', err => {
-  console.log(err.name, ': ', err.message);
+  console.log(err.name, ':', err.message);
   console.log('UNCAUGHT EXCEPTIONS! 💥 Shutting down...');
   process.exit(1);
 });
@@ -18,24 +18,17 @@ const DB = process.env.DB.replace(
   `${username}:${password}`
 );
 
-// connect to database (TESTING COMMIT)
-const connectDB = async () => {
-  try {
-    await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
-};
-connectDB();
+// Connect to Database.
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Connected 🔸'));
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
+  console.log(`App running on port ${PORT}...`);
 });
 
 // Handle all unhandled rejections.
