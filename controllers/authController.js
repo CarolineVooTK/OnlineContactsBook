358af49ae3bea41efb6de150c7841f733fe360d8
qapng <quanganhphucnguyen@gmail.com>
2021-10-15 20:57:34 +0700

    improve user models

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,10 +40,8 @@ const createAndSendToken = (id, res) => {
 // Test function TODO: delete later
 exports.register = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
-    name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+    password: req.body.password
   });
 
   const token = signToken(newUser.id);
