5d91bb02f683c6aba18c90025dc886807d7edff1
May Myat Noe Htet <maymyatnoeht@student.unimelb.edu.au>
2021-10-25 13:42:21 +1100

    Add data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -35,6 +37,12 @@ app.use(limiter);
 app.use(express.json());
 
 app.use(cors());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Mounting routers on routes
 app.use('/api/user', userRouter);
