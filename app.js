e30e78716ccb93153422738e014ad3bb5ce7f94c
May Myat Noe Htet <maymyatnoeht@student.unimelb.edu.au>
2021-10-25 13:36:18 +1100

    Adding HTTP headers security

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,22 +13,30 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
 // Log requests
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Limit request rate to prevent DOS and brute force attacks
+// TODO: Decide on a request rate
 const limiter = rateLimit({
-  max: 1000,
+  max: 200,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use(limiter);
 
+// Body parser
 app.use(express.json());
+
 app.use(cors());
 
+// Mounting routers on routes
 app.use('/api/user', userRouter);
 app.use('/api/contacts', contactRouter);
 app.use('/api/categories', categoryRouter);
@@ -37,6 +46,7 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// Global error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
